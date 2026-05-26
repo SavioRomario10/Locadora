0c5cbb48dcbdd9919effd9fc7437313c3ccda8a3
Sávio Romário <savioromario10@gmail.com>
2026-05-26 15:50:18 -0300

    first commit

---
+++ src/test/java/io/savioromario10/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.savioromario10.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
