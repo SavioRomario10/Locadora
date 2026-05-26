8b78128ed3f70d6cbb37fce9017eb649ec6623f7
Sávio Romário <savioromario10@gmail.com>
2026-05-26 17:32:39 -0300

    adicionando o YAML e ativando os log no projeto

--- src/main/java/io/savioromario10/locadora/Application.java
+++ src/main/java/io/savioromario10/locadora/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocadoraApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(LocadoraApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
